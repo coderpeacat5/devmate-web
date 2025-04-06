1be21b5780aa009e6a4d27360fe9a94303956d77
Gayatri <gayatrisingh2300@gmail.com>
2025-04-07 03:36:14 +0530

    create component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+
+    const user = useSelector((store) => store.user)
+    return (
+        <div className="navbar bg-base-100 shadow-sm">
+            <div className="flex-1">
+                <a className="btn btn-ghost text-xl">DevMate</a>
+            </div>
+            {user && (
+                <div className="flex items-center gap-2">
+                    <p>Welcome, {user.firstName}</p>
+                    <div className="dropdown dropdown-end mr-2">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                            <div className="w-10 rounded-full">
+                                <img
+                                    alt="Tailwind CSS Navbar component"
+                                    src={user.photoUrl} />
+                            </div>
+                        </div>
+                        <ul
+                            tabIndex={0}
+                            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                            <li>
+                                <Link to="/profile" className="justify-between">
+                                    Profile
+                                    <span className="badge">New</span>
+                                </Link>
+                            </li>
+                            <li><a>Settings</a></li>
+                            <li><a>Logout</a></li>
+                        </ul>
+                    </div>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default NavBar
