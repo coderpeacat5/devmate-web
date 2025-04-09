682ccdb95dd4403919418aafa2cb096b8878c9c3
Gayatri <gayatrisingh2300@gmail.com>
2025-04-10 04:03:13 +0530

    add signup form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,22 +6,40 @@ import { addUser } from '../utils/userSlice';
 import { BASE_URL } from '../utils/constants';
 
 const Login = () => {
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
     const [emailId, setEmailId] = useState("gayatri@gmail.com");
-    const [password, setPassword] = useState("Gayatri@12");
+    const [password, setPassword] = useState("Gayatri@123");
     const [error, setError] = useState("");
+    const [isLoginForm, setIsLoginForm] = useState(false)
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
-    const handleClick = async () => {
+    const handleLogin = async () => {
         try {
             const res = await axios.post(
                 BASE_URL + "/login",
                 {
                     emailId, password
-                }, { withCredentials: true })
-            // console.log(res.data)
+                },
+                { withCredentials: true })
             dispatch(addUser(res.data))
-            navigate("/")
+            return navigate("/")
+        } catch (err) {
+            setError(err?.response?.data || "Something went wrong!!");
+        }
+    }
+
+    const handleSignUp = async () => {
+        try {
+            const res = await axios.post(
+                BASE_URL + "/signup",
+                {
+                    firstName, lastName, emailId, password
+                },
+                { withCredentials: true })
+            dispatch(addUser(res.data.data))
+            return navigate("/profile")
         } catch (err) {
             setError(err?.response?.data || "Something went wrong!!");
         }
@@ -31,7 +49,22 @@ const Login = () => {
         <div className='flex justify-center items-center'>
             <div className="card card-border bg-base-200 w-96">
                 <div className="card-body">
-                    <h2 className="card-title justify-center">Login</h2>
+                    <h2 className="card-title justify-center">{isLoginForm ? "Sign In" : "Sign Up"}</h2>
+
+                    {!isLoginForm &&
+                        <>
+                            <fieldset className="fieldset">
+                                <legend className="fieldset-legend text-sm">First Name</legend>
+                                <input type="text" className="input " value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                            </fieldset>
+                            <fieldset className="fieldset">
+                                <legend className="fieldset-legend text-sm">Last Name</legend>
+                                <input type="text"
+                                    className="input"
+                                    value={lastName}
+                                    onChange={(e) => setLastName(e.target.value)} />
+                            </fieldset>
+                        </>}
 
                     <fieldset className="fieldset">
                         <legend className="fieldset-legend text-sm">Email ID</legend>
@@ -48,8 +81,21 @@ const Login = () => {
                     <p className='text-red-500 font-medium mt-2'>{error}</p>
 
                     <div className="card-actions justify-center mt-4">
-                        <button className="btn btn-primary" onClick={handleClick}>Sign In</button>
+                        <button className="btn btn-primary" onClick={isLoginForm ? handleLogin : handleSignUp}>{isLoginForm ? "Sign In" : "Sign Up"}</button>
                     </div>
+
+                    <p onClick={() => setIsLoginForm((value) => !value)}>
+                        {isLoginForm ? (
+                            <>
+                                New to DevMate? <button>Sign Up now</button>
+                            </>
+                        ) : (
+                            <>
+                                Already have an account? <button>Sign In</button>
+                            </>
+                        )}
+                    </p>
+
                 </div>
             </div>
         </div>
