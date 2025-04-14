f5b78e3e9c4337eb9b1c5d62bb240b7f3dc34ec8
Gayatri <gayatrisingh2300@gmail.com>
2025-04-15 03:43:06 +0530

    change

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,44 +1,45 @@
-import { Outlet, useNavigate } from "react-router-dom";
-import NavBar from "./NavBar";
-import Footer from "./Footer";
+import React, { useEffect } from 'react';
+import { BASE_URL } from '../utils/constants';
 import axios from "axios";
 import { useDispatch, useSelector } from "react-redux";
-import { BASE_URL } from "../utils/constants"
-import { addUser } from "../utils/userSlice"
-import { useEffect } from "react";
+import { addUser } from "../utils/userSlice";
+import { useNavigate } from "react-router-dom";
+import NavBar from "./NavBar";
+import Footer from "./Footer";
+import { Outlet } from "react-router-dom";
 
 const Body = () => {
-    const dispatch = useDispatch();
-    const navigate = useNavigate();
-    const userData = useSelector((store) => store.user)
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const userData = useSelector((store) => store.user);
 
-    const fetchUser = async () => {
-        try {
-            if (userData || !localStorage.getItem("token")) return;
-            const res = await axios.get(BASE_URL + "/profile/view", {
-                withCredentials: true
-            })
-            dispatch(addUser(res.data))
-        }
-        catch (err) {
-            if (err.status === 401) {
-                navigate("/login")
-            }
-
-            console.log(err)
-        }
+  const fetchUser = async () => {
+    try {
+      if (userData || !localStorage.getItem("token")) {
+        // Either userData exists or token is missing
+        return;
+      }
+      const res = await axios.get(BASE_URL + "/profile/view", { withCredentials: true });
+      dispatch(addUser(res.data));
+    } catch (err) {
+      if (err.response && err.response.status === 401) {
+        navigate("/login"); // Redirect to login if unauthorized
+      }
+      console.log(err);
     }
+  };
 
-    useEffect(() => {
-        fetchUser()
-    }, [])
-    return (
-        <>
-            <NavBar />
-            <Outlet />
-            {/* <Footer /> */}
-        </>
-    )
-}
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+      {/* <Footer /> */}
+    </>
+  );
+};
 
 export default Body;
--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,12 +1,14 @@
-import React, { useEffect } from 'react'
-import { BASE_URL } from '../utils/constants'
-import axios from "axios"
-import { useDispatch, useSelector } from 'react-redux'
-import { addFeed } from '../utils/feedSlice'
-import UserCard from './UserCard'
+import React, { useEffect } from 'react';
+import { BASE_URL } from '../utils/constants';
+import axios from "axios";
+import { useDispatch, useSelector } from 'react-redux';
+import { addFeed } from '../utils/feedSlice';
+import { useNavigate } from "react-router-dom";
+import UserCard from './UserCard';
 
 const Feed = () => {
-  const feed = useSelector((store) => store.feed)
+  const navigate = useNavigate();
+  const feed = useSelector((store) => store.feed);
   const dispatch = useDispatch();
 
   const getFeed = async () => {
@@ -15,27 +17,26 @@ const Feed = () => {
         return navigate("/login");
       }
       if (feed) return;
-      const res = await axios.get(BASE_URL + "/user/feed",  { withCredentials: true })
-      console.log(res.data)
-      dispatch(addFeed(res?.data))
-    } 
-    catch (err) {
-      console.log(err.message)
+      const res = await axios.get(BASE_URL + "/user/feed", { withCredentials: true });
+      console.log(res.data);
+      dispatch(addFeed(res?.data));
+    } catch (err) {
+      console.log(err.message);
     }
-  }
+  };
 
   useEffect(() => {
-    getFeed()
-  }, [])
+    getFeed();
+  }, []);
 
-  if(!feed) return <h1>Loading...</h1>;
+  if (!feed) return <h1>Loading...</h1>;
 
-  if(feed.length === 0) return <h1 className='flex justify-center font-bold mt-32 text-xl'>No new users found!!</h1>
+  if (feed.length === 0)
+    return (
+      <h1 className="flex justify-center font-bold mt-32 text-xl">No new users found!!</h1>
+    );
 
-  return (
-    feed && (
-    <div className='flex items-center justify-center'><UserCard user={feed[0]}/></div>)
-  )
-}
+  return feed && <div className="flex items-center justify-center"><UserCard user={feed[0]} /></div>;
+};
 
-export default Feed
+export default Feed;
