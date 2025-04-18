6fcb30b95bc01379baa48257dbf96e19ccbf1184
Gayatri <gayatrisingh2300@gmail.com>
2025-04-18 22:43:21 +0530

    change route

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
                 },
                 { withCredentials: true })
             dispatch(addUser(res.data))
-            return navigate("/feed")
+            return navigate("/user/feed")
         } catch (err) {
             setError(err?.response?.data || "Something went wrong!!");
         }
