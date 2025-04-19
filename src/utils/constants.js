d89df014a3a193dde10c74a206b787c313b1e08e
Gayatri <gayatrisingh2300@gmail.com>
2025-04-20 03:59:06 +0530

    add bg-url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,4 @@
-export const BASE_URL = "/api";
+export const BASE_URL = 
+location.hostname === "localhost" ? "http://localhost:7777" : "/api";
+
+export const BG_URL = "https://tinder.com/static/build/8ad4e4299ef5e377d2ef00ba5c94c44c.webp"
