0ff002617953a18b0f0f40a436b45b0e9ab79e96
Gayatri <gayatrisingh2300@gmail.com>
2025-04-25 00:41:45 +0530

    add background image

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import { useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { addUser } from '../utils/userSlice';
-import { BASE_URL } from '../utils/constants';
+import { BASE_URL, BG_URL } from '../utils/constants';
 
 const Login = () => {
     const [firstName, setFirstName] = useState("");
@@ -11,7 +11,7 @@ const Login = () => {
     const [emailId, setEmailId] = useState("gayatri@gmail.com");
     const [password, setPassword] = useState("Gayatri@123");
     const [error, setError] = useState("");
-    const [isLoginForm, setIsLoginForm] = useState(false)
+    const [isLoginForm, setIsLoginForm] = useState(true)
     const dispatch = useDispatch();
     const navigate = useNavigate();
 
@@ -47,20 +47,23 @@ const Login = () => {
 
     return (
         <div className='flex justify-center items-center'>
-            <div className="card card-border bg-base-200 w-96">
+            <div className='absolute inset-0 w-full h-full overflow-x-hidden overflow-y-auto'>
+                <img src={BG_URL} alt='background-image' className='w-full h-full object-cover bg-repeat-round' />
+            </div>
+            <div className="card card-border w-80 bg-black bg-opacity-80  mt-16">
                 <div className="card-body">
-                    <h2 className="card-title justify-center">{isLoginForm ? "Sign In" : "Sign Up"}</h2>
+                    <h2 className="card-title justify-center ">{isLoginForm ? "Sign In" : "Sign Up"}</h2>
 
                     {!isLoginForm &&
                         <>
                             <fieldset className="fieldset">
                                 <legend className="fieldset-legend text-sm">First Name</legend>
-                                <input type="text" className="input " value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                                <input type="text" className="input focus:outline-none" value={firstName} onChange={(e) => setFirstName(e.target.value)} />
                             </fieldset>
                             <fieldset className="fieldset">
                                 <legend className="fieldset-legend text-sm">Last Name</legend>
                                 <input type="text"
-                                    className="input"
+                                    className="input focus:outline-none"
                                     value={lastName}
                                     onChange={(e) => setLastName(e.target.value)} />
                             </fieldset>
@@ -68,30 +71,30 @@ const Login = () => {
 
                     <fieldset className="fieldset">
                         <legend className="fieldset-legend text-sm">Email ID</legend>
-                        <input type="text" className="input " value={emailId} onChange={(e) => setEmailId(e.target.value)} />
+                        <input type="text" className="input focus:outline-none" value={emailId} onChange={(e) => setEmailId(e.target.value)} />
                     </fieldset>
                     <fieldset className="fieldset">
                         <legend className="fieldset-legend text-sm">Password</legend>
                         <input type="text"
-                            className="input"
+                            className="input focus:outline-none mb-0"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)} />
                     </fieldset>
 
                     <p className='text-red-500 font-medium mt-2'>{error}</p>
 
-                    <div className="card-actions justify-center mt-4">
+                    <div className="card-actions justify-center mt-1">
                         <button className="btn btn-primary" onClick={isLoginForm ? handleLogin : handleSignUp}>{isLoginForm ? "Sign In" : "Sign Up"}</button>
                     </div>
 
                     <p onClick={() => setIsLoginForm((value) => !value)}>
                         {isLoginForm ? (
                             <div className='mt-2'>
-                                New to DevMate? <button className='font-bold'>Sign Up now</button>
+                                New to DevMate? <button className='font-semibold text-blue-300 hover:text-blue-400'>Sign Up now</button>
                             </div>
                         ) : (
                             <div className='mt-2'>
-                                Already have an account? <button className='font-bold'>Sign In</button>
+                                Already have an account? <button className='font-semibold text-blue-300 hover:text-blue-400'>Sign In</button>
                             </div>
                         )}
                     </p>
